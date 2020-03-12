a441e5e0f12c856612b71ef303fa54b2b4a21a0e
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-12 16:27:54 +0000

    refactor all

--- users/userRouter.js
+++ users/userRouter.js
@@ -3,20 +3,7 @@ const users = require("./userDb");
 const posts = require("../posts/postDb");
 const router = express.Router();
 
-//posting new user using promise chains 
-router.post("/", validateUser(), (req, res, next) => {
-  users
-    .insert(req.body)
-    .then(user => {
-      res.status(201).json(user);
-    })
-    .catch(err => {
-      next(err);
-    });
-});
-
-//USING ASYNC
-
+//posting new user using promise chains
 // router.post("/", validateUser(), (req, res, next) => {
 //   users
 //     .insert(req.body)
@@ -28,23 +15,35 @@ router.post("/", validateUser(), (req, res, next) => {
 //     });
 // });
 
+//USING ASYNC
 
-//ADDING a new post for a specific user 
-router.post("/:id/posts", validateUserId(), validatePost(),(req, res, next) => {
-const objectToSend = { text: req.body.text, user_id: req.params.id }
-
-posts.insert(objectToSend)
-.then(post => {
-  res.status(201).json(post)
-})
-.catch(err => {
-  next(err)
-})
-
+router.post("/", validateUser(), async (req, res, next) => {
+  try {
+    const user = await users.insert(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    next(err);
+  }
 });
 
+//ADDING a new post for a specific user
+router.post(
+  "/:id/posts",
+  validateUserId(),
+  validatePost(),
+  async (req, res, next) => {
+    const objectToSend = { text: req.body.text, user_id: req.params.id };
 
-//PROMISE CHAIN WAY 
+    try {
+      const post = await posts.insert(objectToSend);
+      res.status(201).json(post);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+//PROMISE CHAIN WAY
 
 // router.get("/", (req, res, next) => {
 //   // res.send('Hello this works')
@@ -58,37 +57,29 @@ posts.insert(objectToSend)
 //     });
 // });
 
-
 //USING ASYNC
 
 router.get("/", async (req, res, next) => {
-try {
-  const user = await users.get()
-  res.status(200).json(user);
-} catch(err) {
-      next(err);
-}
+  try {
+    const user = await users.get();
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
 });
-
-
 
 // router.get("/:id", validateUserId(), (req, res) => {
 //     res.status(200).json(req.user);
 
 // });
 
-
 router.get("/:id", validateUserId(), async (req, res) => {
-
   try {
     res.status(200).json(req.user);
   } catch (error) {
-    next(error)
+    next(error);
   }
-  
-
 });
-
 
 // router.get("/:id/posts", validateUserId(), (req, res, next) => {
 
@@ -102,18 +93,14 @@ router.get("/:id", validateUserId(), async (req, res) => {
 
 // });
 
-
 router.get("/:id/posts", validateUserId(), async (req, res, next) => {
-try {
-  const posts = await users.getUserPosts(req.params.id)
-  res.status(200).json(posts);
-
-} catch (error) {
-  next(err);
-}
-
+  try {
+    const posts = await users.getUserPosts(req.params.id);
+    res.status(200).json(posts);
+  } catch (error) {
+    next(err);
+  }
 });
-
 
 // router.delete("/:id", validateUserId(), (req, res, next) => {
 //   users.remove(req.params.id)
@@ -132,24 +119,22 @@ try {
 
 router.delete("/:id", validateUserId(), async (req, res, next) => {
   try {
-    const count = await users.remove(req.params.id)
+    const count = await users.remove(req.params.id);
     if (count > 0) {
-      res.status(200).json({ message: 'The user has been deleted' });
+      res.status(200).json({ message: "The user has been deleted" });
     } else {
-      res.status(404).json({ message: 'The user could not be found' });
+      res.status(404).json({ message: "The user could not be found" });
     }
-    
   } catch (error) {
-    next(error)
+    next(error);
   }
-  
 });
 
-//changing user info 
+//changing user info
 // router.put("/:id", validateUserId(), validateUser(), (req, res, next) => {
 // users.update(req.params.id, req.body)
 // .then(user => {
-//     res.status(200).json(user);  
+//     res.status(200).json(user);
 // })
 // .catch(error => {
 //   next(error)
@@ -158,52 +143,46 @@ router.delete("/:id", validateUserId(), async (req, res, next) => {
 // });
 
 router.put("/:id", validateUserId(), validateUser(), async (req, res, next) => {
-try {
-  const user = await users.update(req.params.id, req.body)
-  res.status(200).json(user); 
-} catch (error) {
-  next(error)
-}
-
-  });
-
+  try {
+    const user = await users.update(req.params.id, req.body);
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+});
 
 //Custom validation MiddleWare functions:
 
 function validateUserId() {
   // if the `id` parameter is valid, store that user object as `req.user`
   // if the `id` parameter does not match any user id in the database, cancel the request and respond with status `400` and `{ message: "invalid user id" }`
-return (req, res, next) => {
-  users.getById(req.params.id)
-  .then((user) => {
-    if(user) {
-      req.user = user
-      next()
-    } else {
-      res.status(400).json({message: "invalid user id"})
-    }
-  })
-  .catch(err => {
-    next(err)
-  })
-}
-
+  return (req, res, next) => {
+    users
+      .getById(req.params.id)
+      .then(user => {
+        if (user) {
+          req.user = user;
+          next();
+        } else {
+          res.status(400).json({ message: "invalid user id" });
+        }
+      })
+      .catch(err => {
+        next(err);
+      });
+  };
 }
 
 function validatePost() {
   return (req, res, next) => {
-    if(!req.body) {
-       res.status(400).json({message: "missing post data"})
-    }
-    else if(!req.body.text) {
-       res.status(400).json({message: "missing required text field" })
-
-    }
-    else {
-    next()
+    if (!req.body) {
+      res.status(400).json({ message: "missing post data" });
+    } else if (!req.body.text) {
+      res.status(400).json({ message: "missing required text field" });
+    } else {
+      next();
     }
   };
-
 }
 
 function validateUser() {
