7a87ce34df1a8295f8477485238e09d4c896ab04
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-12 16:21:26 +0000

    refactored using async await, commented out promise chain way

--- users/userRouter.js
+++ users/userRouter.js
@@ -3,7 +3,7 @@ const users = require("./userDb");
 const posts = require("../posts/postDb");
 const router = express.Router();
 
-//posting new user
+//posting new user using promise chains 
 router.post("/", validateUser(), (req, res, next) => {
   users
     .insert(req.body)
@@ -15,7 +15,21 @@ router.post("/", validateUser(), (req, res, next) => {
     });
 });
 
-//posting a new post for a specific user 
+//USING ASYNC
+
+// router.post("/", validateUser(), (req, res, next) => {
+//   users
+//     .insert(req.body)
+//     .then(user => {
+//       res.status(201).json(user);
+//     })
+//     .catch(err => {
+//       next(err);
+//     });
+// });
+
+
+//ADDING a new post for a specific user 
 router.post("/:id/posts", validateUserId(), validatePost(),(req, res, next) => {
 const objectToSend = { text: req.body.text, user_id: req.params.id }
 
@@ -30,65 +44,128 @@ posts.insert(objectToSend)
 });
 
 
-router.get("/", (req, res, next) => {
-  // res.send('Hello this works')
-  users
-    .get()
-    .then(user => {
-      res.status(200).json(user);
-    })
-    .catch(err => {
+//PROMISE CHAIN WAY 
+
+// router.get("/", (req, res, next) => {
+//   // res.send('Hello this works')
+//   users
+//     .get()
+//     .then(user => {
+//       res.status(200).json(user);
+//     })
+//     .catch(err => {
+//       next(err);
+//     });
+// });
+
+
+//USING ASYNC
+
+router.get("/", async (req, res, next) => {
+try {
+  const user = await users.get()
+  res.status(200).json(user);
+} catch(err) {
       next(err);
-    });
-});
-
-router.get("/:id", validateUserId(), (req, res) => {
-res.status(200).json(req.user);
-
+}
 });
 
 
-router.get("/:id/posts", validateUserId(), (req, res, next) => {
 
-  users.getUserPosts(req.params.id)
-  .then(posts => {
-    res.status(200).json(posts);
-  })
-  .catch(err => {
-    next(err);
-  })
+// router.get("/:id", validateUserId(), (req, res) => {
+//     res.status(200).json(req.user);
+
+// });
+
+
+router.get("/:id", validateUserId(), async (req, res) => {
+
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    next(error)
+  }
+  
 
 });
 
 
-router.delete("/:id", validateUserId(), (req, res, next) => {
-  users.remove(req.params.id)
-  .then(count => {
+// router.get("/:id/posts", validateUserId(), (req, res, next) => {
+
+//   users.getUserPosts(req.params.id)
+//   .then(posts => {
+//     res.status(200).json(posts);
+//   })
+//   .catch(err => {
+//     next(err);
+//   })
+
+// });
+
+
+router.get("/:id/posts", validateUserId(), async (req, res, next) => {
+try {
+  const posts = await users.getUserPosts(req.params.id)
+  res.status(200).json(posts);
+
+} catch (error) {
+  next(err);
+}
+
+});
+
+
+// router.delete("/:id", validateUserId(), (req, res, next) => {
+//   users.remove(req.params.id)
+//   .then(count => {
+//     if (count > 0) {
+//       res.status(200).json({ message: 'The user has been deleted' });
+//     } else {
+//       res.status(404).json({ message: 'The user could not be found' });
+//     }
+//   })
+//   .catch(error => {
+//     next(error)
+//   });
+
+// });
+
+router.delete("/:id", validateUserId(), async (req, res, next) => {
+  try {
+    const count = await users.remove(req.params.id)
     if (count > 0) {
       res.status(200).json({ message: 'The user has been deleted' });
     } else {
       res.status(404).json({ message: 'The user could not be found' });
     }
-  })
-  .catch(error => {
+    
+  } catch (error) {
     next(error)
-  });
-
-
-
+  }
+  
 });
 
 //changing user info 
-router.put("/:id", validateUserId(), validateUser(), (req, res, next) => {
-users.update(req.params.id, req.body)
-.then(user => {
-    res.status(200).json(user);  
-})
-.catch(error => {
-  next(error)
-});
+// router.put("/:id", validateUserId(), validateUser(), (req, res, next) => {
+// users.update(req.params.id, req.body)
+// .then(user => {
+//     res.status(200).json(user);  
+// })
+// .catch(error => {
+//   next(error)
+// });
 
-});
+// });
+
+router.put("/:id", validateUserId(), validateUser(), async (req, res, next) => {
+try {
+  const user = await users.update(req.params.id, req.body)
+  res.status(200).json(user); 
+} catch (error) {
+  next(error)
+}
+
+  });
 
 
 //Custom validation MiddleWare functions:
